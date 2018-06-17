7f1671965854566464e5a73b418e9dcf57cab497
Haoyang Chen <haoychen@outlook.com>
2018-06-17 00:04:27 -0700

    Restructure the repo

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <tbb/tbb.h>
-#include <opencv/cv.h>
-#include <rapidjson/rapidjson.h>
+//#include <tbb/tbb.h>
+//#include <opencv/cv.h>
+//#include <rapidjson/rapidjson.h>
 #include <boost/any.hpp>
-#include <CUDA/cuda.h>
+//#include <CUDA/cuda.h>
 
 
 using namespace std;
