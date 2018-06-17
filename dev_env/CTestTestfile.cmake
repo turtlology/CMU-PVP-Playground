ac1b8db88256b0209eab8c7aa6efffa2d81aca24
Haoyang Chen <haoychen@outlook.com>
2018-06-16 22:42:00 -0700

    remove useless files && re-structured repo

--- dev_env/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/turtlologist/git/CMU-PVP-Playground/dev_env
-# Build directory: /Users/turtlologist/git/CMU-PVP-Playground/dev_env
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tester "tester")
